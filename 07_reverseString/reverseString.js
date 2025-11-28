a56adda1132296026c3a5f636923159d1a0737c7
Jorge Moreno <jmoreno@gatech.edu>
2025-11-27 20:51:09 -0800

    Complete 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let reversed = '';
+    for (i = 1; i <= string.length; i++) {
+        reversed += string[string.length - i];
+    }
+
+    return reversed;
+
 
 };
 
