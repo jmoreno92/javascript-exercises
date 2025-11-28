bda9e082d9ee508d50698b58c0e174625a005e71
Jorge Moreno <jmoreno@gatech.edu>
2025-11-27 20:38:35 -0800

    Complete 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+    let string = '';
 
+    if (times < 0) {
+        return "ERROR";
+    }
+
+    if (times == 0) {
+        return string;
+
+    } else {
+        for (let i = 0; i < times; i++) {
+            string += word;
+        }
+        return string;
+    }
 };
 
 // Do not edit below this line
