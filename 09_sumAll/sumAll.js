9cdbc10cc422dfbabb4fd404478396a3f8732ee8
Jorge Moreno <jmoreno@gatech.edu>
2025-11-27 21:53:56 -0800

    Complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
+
+    let num = 0;
+
+    for (i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+        num += i;
+    }
+
+    return num;
 
 };
 
